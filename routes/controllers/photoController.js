b2346cc0a15f7ab8395e94025a502d45a9a4ca79
idbd <idbd.young@gmail.com>
2020-11-25 21:21:15 +0900

    refactor: change logic to get photo by location

--- routes/controllers/photoController.js
+++ routes/controllers/photoController.js
@@ -4,9 +4,13 @@ const { statusMessage } = require('../../constants/statusMessage');
 
 exports.getPhotoByLocation = async (req, res, next) => {
   try {
-    const { latitude, longitude } = req.query;
-
-    const photos = await PhotoService.getPhotoByLocation(latitude, longitude);
+    const {
+      limit,
+      page,
+      latitude,
+      longitude
+    } = req.query;
+    const photos = await PhotoService.getPhotoByLocation(latitude, longitude, limit, page);
 
     return res.status(200).json({
       result: statusMessage.success,
